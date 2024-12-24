8af583721d6ea9da97f9d52df19710542d551310
Adebayo Adebanjo <akaboyo@gmail.com>
2024-12-24 10:35:35 +0100

    Update 01_08_challenge.sql

--- Chapter 1/01_08_challenge.sql
+++ Chapter 1/01_08_challenge.sql
@@ -1,1 +1,23 @@
 -- Insert new customer into Customer table
+INSERT INTO Customer (
+    CustomerID,
+    FirstName,
+    LastName,
+    Email,
+    Phone,
+    Address,
+    City,
+    State,
+    Zipcode
+  )
+VALUES (
+    1100,
+    'Bayo',
+    'Adebanjo',
+    'akaboyo@gmail.com',
+    '(912)459-2910',
+    '20 Ayonuga Str',
+    'Lagos City',
+    'LAG',
+    '64161'
+  );
